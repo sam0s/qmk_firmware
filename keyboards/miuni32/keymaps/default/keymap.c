1d31c0c216eec1bfbbc5dc4d266de640714a3128
Adam YH Lee <adam.yh.lee@gmail.com>
2017-03-13 08:36:28 -0700

    add unused key

--- keyboards/miuni32/keymaps/default/keymap.c
+++ keyboards/miuni32/keymaps/default/keymap.c
@@ -4,7 +4,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] ={
        {KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC},
        {KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_RSFT, KC_ENT},
-       {KC_Z,   KC_X,   KC_C,   KC_V,   KC_NO,  KC_SPC, KC_B,   KC_N,   KC_M ,  MO(1), KC_RCTL}
+       {KC_Z,   KC_X,   KC_C,   KC_V,   KC_V,  KC_SPC, KC_B,   KC_N,   KC_M ,  MO(1), KC_RCTL}
      },
 [1] ={
        {KC_PGUP,  KC_UP,    KC_PGDN,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_DEL},
